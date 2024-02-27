67488d64b07aed07e646de87b6f2bede157cea12
AStefankiv <stefankif35@gmail.com>
2024-02-26 21:29:03 -0700

    Replaced PhotoListItem with PhotoList

--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -59,7 +59,22 @@ const sampleDataForPhotoList = [
 const PhotoList = () => {
   return (
     <ul className="photo-list">
-      {/* Insert React */}
+      {sampleDataForPhotoList.map((photo, index) => (
+        <li key={index} className='photo-list__item'>
+          <img className="photo-list__image" src={photo.urls.regular} alt="Photo"
+          />
+          <div className="photo-list__user-details">
+            <img className="photo-list__user-profile" src={photo.user.profile} alt='Profile' />
+            <div className="photo-list__user-info">
+              {photo.user.username}
+              <div className="photo-list__user-location">
+                {photo.location.city}, {photo.location.country}
+              </div>
+            </div>
+          </div>
+        </li>
+      )
+      )}
     </ul>
   );
 };
