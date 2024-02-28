35e6e3614db6905448786dcdbaa0c776baaa7c03
AStefankiv <stefankif35@gmail.com>
2024-02-27 18:53:05 -0700

    Photo topics, improved

--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -2,15 +2,10 @@ import React from "react";
 
 import "../styles/TopicListItem.scss";
 
-const sampleDataForTopicListItem = {
-  id: "1",
-  slug: "topic-1",
-  label: "Nature",
-};
 
 const TopicListItem = ({ topic }) => {
   return (
-    <li key={topic.id} className="topic-list__item">{topic.title}</li>
+    <li className="topic-list__item">{topic.title}</li>
   );
 };
 
