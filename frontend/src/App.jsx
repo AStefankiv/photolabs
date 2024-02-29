cb744e5fdcf76e6181f843bb04fc75b167755ad0
AStefankiv <stefankif35@gmail.com>
2024-02-28 18:44:46 -0700

    Consumed mock data to populate the photos and topics

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,16 @@
 import React from 'react';
 
-// import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 import HomeRoute from 'components/HomeRoute';
+import topics from './mocks/topics';
 
-const sampleDataForTopicList = [
-  {id: "1", slug: "topic-1", title: "Nature",},
-  {id: "2", slug: "topic-2", title: "Travel",},
-  {id: "3", slug: "topic-3", title: "People",},
-];
-// const photos = new Array(3).fill(sampleDataForPhotoListItem);
+
 
 const App = () => {
   return (
     <div className="App">
       <div className="photo-list">
-        <HomeRoute topics={sampleDataForTopicList}/>
+        <HomeRoute topics={topics} />
       </div>
     </div>
   );
